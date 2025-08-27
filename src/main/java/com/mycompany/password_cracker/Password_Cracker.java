010eab875f70f666e0f5f9715e959be282816b40
Ashmit sharma <ramanghyu@gmail.com>
2025-08-27 10:55:52 +0530

    Initial Commit

---
+++ src/main/java/com/mycompany/password_cracker/Password_Cracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.password_cracker;
+
+/**
+ *
+ * @author Ashmit Sharma
+ */
+public class Password_Cracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
